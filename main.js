f8e92170e9874be43870063f096f41d7c60c873e
yusif <yusif.aliyev.25@mail.ru>
2024-01-02 23:26:31 +0400

    design and logic

--- main.js
+++ main.js
@@ -1,27 +1,81 @@
-const card = document.querySelector("#card")
+const weathercon = document.querySelector("#weathercon");
+const locations = document.querySelector("#locations");
+const date  = document.querySelector("#date");
+const temp = document.querySelector("#temp");
+const btnSrc = document.querySelector("#btnSrc");
+const srcLine = document.querySelector("#srcLine");
+
+srcLine.addEventListener("keypress", event=>{
+    if(event.key === "Enter"){
+        event.preventDefault();
+        btnSrc.click()
+    }
+})
+btnSrc.addEventListener("click", function(){
+    const cityName = srcLine.value;
+    console.log(cityName);
+    const names = fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&APPID=35b1f1d45a7b4378cf2430ae601816be&units=metric`)
+names.then(res =>{
+    const promise = res.json();
+    return promise
+}).then(namez).catch(err=>{
+    console.log(err);
+})
+
+})
+
+
 
 const myPromise = fetch("https://api.openweathermap.org/data/2.5/weather?q=baku&APPID=35b1f1d45a7b4378cf2430ae601816be&units=metric")
 myPromise.then(res =>{
     const promise = res.json();
     return promise
-}).then(promise=>{
-    console.log(promise);
+}).then(data=>{
+    console.log(data);
 }).catch(err=>{
     console.log(err);
-})
+});
+
+const mains = fetch("https://api.openweathermap.org/data/2.5/weather?q=baku&APPID=35b1f1d45a7b4378cf2430ae601816be&units=metric")
+mains.then(res =>{
+    const promise = res.json();
+    return promise
+}).then(main).catch(err=>{
+    console.log(err);
+});
+function main(data){
+    const degre = data.main;
+    console.log(degre);
+    temp.innerHTML = degre.temp
+
+    
+    
+}
+const weathers = fetch("https://api.openweathermap.org/data/2.5/weather?q=baku&APPID=35b1f1d45a7b4378cf2430ae601816be&units=metric")
+weathers.then(res =>{
+    const promise = res.json();
+    return promise
+}).then(weather).catch(err=>{
+    console.log(err);
+});
 
 
 
-// function renderItem(data){
-//     const paqoda = data.weather;
-//     console.log(paqoda);
+
+function weather(data){
+    const paqoda = data.weather;
+    console.log(paqoda);
+   
 
 
-//     card.innerHTML = paqoda.map(item => `img src="${item.icon}" class="card-img-top" alt="...">
-//     <div class="card-body">
-//       <h5 class="card-title">${item.main}</h5>
-//       <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-//       <a href="#" class="btn btn-primary">Go somewhere</a>
-//     </div>`)
+}
 
-// }
+
+
+function namez(data){
+    const name = data.name;
+    locations.innerHTML = name
+    console.log(name);
+    
+    
+}
